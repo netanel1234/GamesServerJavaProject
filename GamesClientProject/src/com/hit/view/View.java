7ff61c97d8cc7e30ba3a6e645a22bbba6ca74f44
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 14:16:09 +0200

    Update View.java

--- GamesClientProject/src/com/hit/view/View.java
+++ GamesClientProject/src/com/hit/view/View.java
@@ -2,17 +2,22 @@ package com.hit.view;
 
 public interface View {
 	
-	//Starts the view
+	/**
+	 * Starts the view
+	 */
 	void start();
 	
-	//Update the view when a new-game response message arrives
-	//board - the initial game board
+	/**
+	 * Update the view when a new-game response message arrives
+	 * @param board - the initial game board
+	 */
 	void updateViewNewGame(java.lang.Character[] board);
 	
-	//Updates the game board with the last move
-	//Parameters:
-	//gameState - the state of the game after the last round
-	//board - the board state after the last round
+	/**
+	 * Updates the game board with the last move
+	 * @param gameState - the state of the game after the last round
+	 * @param board - the board state after the last round
+	 */
 	void updateViewGameMove(int gameState,Character[] board);
 	
 }
