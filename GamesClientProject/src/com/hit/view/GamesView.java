d3c90cb8cdec7a952c0f9b688279362a3710ae0b
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 14:16:33 +0200

    Update GamesView.java

--- GamesClientProject/src/com/hit/view/GamesView.java
+++ GamesClientProject/src/com/hit/view/GamesView.java
@@ -1,6 +1,5 @@
 package com.hit.view;
 
-
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -20,31 +19,28 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
-
-
+/**
+ * GamesView contains the graphic interface of the system and creates it when the system is running 
+ */
 public class GamesView extends JPanel implements View, ActionListener, ItemListener {
 	
 	private static final long serialVersionUID = 1L;
-	
-	JFrame frame;
-	JPanel opponentPanel,typeGamePanel,gameBoardPanel,mainPanel,boardPanel;
-	JButton newGameButton;
-	JButton[] boardButtons;
-	JLabel opponentLabel,gameLabel;
-	JRadioButton smartButton,randomButton,ticTacTowButton,catcTheBunnyButton;
-	boolean smartChoosen,randomChoosen,tttChoosen,ctbChoosen;
-	Icon xIcon,circleIcon,boyIcon,bunnyIcon;
-	
-	PropertyChangeSupport propertyChangeHandler;
+	private JFrame frame;
+	private JPanel opponentPanel, typeGamePanel, gameBoardPanel, mainPanel, boardPanel;
+	private JButton newGameButton;
+	private JButton[] boardButtons;
+	private JLabel opponentLabel, gameLabel;
+	private JRadioButton smartButton, randomButton, ticTacTowButton, catcTheBunnyButton;
+	private boolean smartChoosen, randomChoosen, tttChoosen, ctbChoosen;
+	private Icon xIcon, circleIcon, boyIcon, bunnyIcon;
+	private PropertyChangeSupport propertyChangeHandler;
 	
 	public GamesView()
-	{
-		System.out.println("constractor of GamesView");
-		
-		xIcon=new ImageIcon("C:\\Users\\netan\\eclipse-workspace\\GamesClientProject\\src\\icons\\x.png");
-		circleIcon=new ImageIcon("C:\\Users\\netan\\eclipse-workspace\\GamesClientProject\\src\\icons\\circle.png");
-		boyIcon=new ImageIcon("C:\\Users\\netan\\eclipse-workspace\\GamesClientProject\\src\\icons\\boy.png");
-		bunnyIcon=new ImageIcon("C:\\Users\\netan\\eclipse-workspace\\GamesClientProject\\src\\icons\\bunny.png");
+	{		
+		xIcon=new ImageIcon("icons\\x.png");
+		circleIcon=new ImageIcon("icons\\circle.png");
+		boyIcon=new ImageIcon("icons\\boy.png");
+		bunnyIcon=new ImageIcon("icons\\bunny.png");
 		
 		smartChoosen=true;
 		randomChoosen=false;
@@ -107,16 +103,16 @@ public class GamesView extends JPanel implements View, ActionListener, ItemListe
 						
 		setOpaque(true);
 		frame.setContentPane(this);
-		//frame.setPreferredSize(new Dimension(500,200));
 	}
 	
 	public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener)
 	{
-		System.out.println("addPropertyChangeListener in GamesView");
 		propertyChangeHandler.addPropertyChangeListener(propertyChangeListener);
 	}
 	
-	//Starts the view
+	/**
+	 * start in interface View
+	 */
 	@Override
 	public void start() 
 	{
@@ -128,21 +124,15 @@ public class GamesView extends JPanel implements View, ActionListener, ItemListe
 		});
 	}
 	
-	//Update the view when a new-game response message arrives
-	//board - the initial game board
+	/**
+	 * updateViewNewGame in interface View
+	 * @param board - the initial game board
+	 */
 	@Override
 	public void updateViewNewGame(Character[] board) 
-	{
-		System.out.println("in GamesView in method updateViewNewGame. printting the board->");
-		for(Character c:board)
-		{
-			System.out.println(c);
-		}
-				
+	{	
 		boardPanel=new JPanel();
-		
 		int row=board.length;
-		
 		if(row==9)
 		{
 			boardButtons=new JButton[9];
@@ -153,25 +143,16 @@ public class GamesView extends JPanel implements View, ActionListener, ItemListe
 			{
 				JButton button=null;
 				if(board[i]=='b')
-				{
 					button=new JButton("");
-				}
-				else if(board[i]=='p')
-				{
-					button=new JButton(circleIcon);
-				}
-				else if(board[i]=='c')
-				{
-					button=new JButton(xIcon);
-				}
+		
 				button.setPreferredSize(new Dimension(50,50));
-				button.setActionCommand("tttupdateGameMoveView"+i);
+				button.setActionCommand("tttupdateGameMoveView "+i);
 				button.addActionListener(this);
 				boardButtons[i]=button;
 				boardPanel.add(button);
 			}
 		}
-		else// if board.length is 81.
+		else
 		{
 			boardButtons=new JButton[81];
 			row=9;
@@ -181,82 +162,70 @@ public class GamesView extends JPanel implements View, ActionListener, ItemListe
 			{
 				JButton button=null;
 				if(board[i]=='b')
-				{
 					button=new JButton();
-				}
 				else if(board[i]=='p')
-				{
 					button=new JButton(boyIcon);
-				}
 				else if(board[i]=='c')
-				{
 					button=new JButton(bunnyIcon);
-				}
+				
 				button.setPreferredSize(new Dimension(50,50));
-				button.setActionCommand("ctbupdateGameMoveView"+i);
+				button.setActionCommand("ctbupdateGameMoveView "+i);
 				button.addActionListener(this);
 				boardButtons[i]=button;
 				boardPanel.add(button);
 			}
 		}
-		
 		add(boardPanel);
 	}
 	
-	//Updates the game board with the last move
-	//gameState - the state of the game after the last round
-	//board - the board state after the last round
+	/**
+	 * updateViewGameMove in interface View
+	 * Updates the game board with the last move
+	 * @param gameState - the state of the game after the last round
+	 * @param board - the board state after the last round
+	 */
 	@Override
 	public void updateViewGameMove(int gameState, Character[] board) 
 	{
-		
+		for(int i=0;i<9;i++)
+		{
+			if(board[i]=='p')
+				boardButtons[i].setText("X");
+			else if(board[i]=='c')
+				boardButtons[i].setText("O");
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) 
 	{
-		System.out.println("actionPerformed in GamesView");
 		if("newGame".equals(event.getActionCommand()))
 		{
-			//Check if the user select only one type of game and only one type of opponent.
 			if(smartChoosen==true && randomChoosen==false && tttChoosen==true && ctbChoosen==false)
-			{
 				propertyChangeHandler.firePropertyChange("newGameView",0,1);
-				System.out.println("in actionPerformed in GamesView the game selected is ttt smart");
-			}
 			else if(smartChoosen==false && randomChoosen==true && tttChoosen==true && ctbChoosen==false)
-			{
 				propertyChangeHandler.firePropertyChange("newGameView",0,2);
-			}
 			else if(smartChoosen==true && randomChoosen==false && tttChoosen==false && ctbChoosen==true)
-			{
 				propertyChangeHandler.firePropertyChange("newGameView",0,3);
-			}
 			else if(smartChoosen==false && randomChoosen==true && tttChoosen==false && ctbChoosen==true)
-			{
 				propertyChangeHandler.firePropertyChange("newGameView",0,4);
-			}
 		}
-		else if(event.getActionCommand().contains("ctbUpdateGameMoveView"))
-		{
-			String[] arr=event.getActionCommand().split("MoveView");
-			
-			System.out.println("arr[1]->"+arr[1]);
+		else if(event.getActionCommand().contains("ctbupdateGameMoveView"))
+		{			
+			String[] arr=event.getActionCommand().split(" ");
 			int buttonNum=Integer.parseInt(arr[1]);
 			int row=buttonNum/9;
 			int col=buttonNum%9;
-			System.out.println("row->"+row+" col->"+col);
 			propertyChangeHandler.firePropertyChange("updatePlayerMoveView",row,col);
 		}
-		else if(event.getActionCommand().contains("tttUpdateGameMoveView"))
-		{
-			String[] arr=event.getActionCommand().split("MoveView");
-			
-			System.out.println("arr[1]->"+arr[1]);
+		else if(event.getActionCommand().contains("tttupdateGameMoveView"))
+		{			
+			String[] arr=event.getActionCommand().split(" ");
 			int buttonNum=Integer.parseInt(arr[1]);
 			int row=buttonNum/3;
 			int col=buttonNum%3;
-			System.out.println("row->"+row+" col->"+col);
+			if(row==col)
+				row=-1;
 			propertyChangeHandler.firePropertyChange("updatePlayerMoveView",row,col);
 		}
 	}
@@ -268,51 +237,31 @@ public class GamesView extends JPanel implements View, ActionListener, ItemListe
 		if(source==smartButton)
 		{
 			if(smartChoosen==true)
-			{
 				smartChoosen=false;
-			}
 			else
-			{
 				smartChoosen=true;
-			}
 		}
 		else if(source==randomButton)
 		{
 			if(randomChoosen==true)
-			{
 				randomChoosen=false;
-			}
 			else
-			{
 				randomChoosen=true;
-			}
 		}
 		else if(source==ticTacTowButton)
 		{
 			if(tttChoosen==true)
-			{
 				tttChoosen=false;
-			}
 			else
-			{
 				tttChoosen=true;
-			}
 		}
 		else if(source==catcTheBunnyButton)
 		{
 			if(ctbChoosen==true)
-			{
 				ctbChoosen=false;
-			}
 			else
-			{
 				ctbChoosen=true;
-			}
 		}
 	}
 	
-	
-	
-	
-
 }
