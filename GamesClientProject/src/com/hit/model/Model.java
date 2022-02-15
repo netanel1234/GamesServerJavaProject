d011036facbd7b1e450495eb0e1c6cfc337653d3
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 13:55:37 +0200

    Update Model.java

--- GamesClientProject/src/com/hit/model/Model.java
+++ GamesClientProject/src/com/hit/model/Model.java
@@ -2,6 +2,17 @@ package com.hit.model;
 
 public interface Model {
 	
+	/**
+	 * Sends the server a "new-game" request
+	 * @param gameType - the chosen game
+	 * @param opponentType - computer type (random \ smart)
+	 */
 	void newGame(String gameType,String opponentType);
+	
+	/**
+	 * Sends the server an "update-move" message
+	 * @param row - the row of the move the player chose
+	 * @param col - the column of the move the player chose
+	 */
 	void updatePlayerMove(int row,int col);
 }
