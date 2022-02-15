06cb14883df656b4641d47478c163e44092deb9d
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 14:21:42 +0200

    Update CLI.java

--- GamesServerProject/src/com/hit/util/CLI.java
+++ GamesServerProject/src/com/hit/util/CLI.java
@@ -10,32 +10,56 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+/**
+ * This class responsible to interacting with the client 
+ * in order to run the server and stop it if necessary.
+ */
 public class CLI implements Runnable {
+	
+	/**
+	 * Representation of all possible commands to the server from the client 
+	 */
+	enum InputValidation
+	{
+		CONFIG, DEFAULT, START, SHUTDOWN, INVALID;
+	}
 
-	PropertyChangeSupport propertyChangeHandler;
-	BufferedReader in;
-	BufferedWriter out;
+	private PropertyChangeSupport propertyChangeHandler;
+	private BufferedReader in;
+	private BufferedWriter out;
 
+	/**
+	 * Constructor 
+	 * @param in - the stream from which the CLI commands are received
+	 * @param out - the stream that get the CLI responses to the received commands
+	 */
 	public CLI(InputStream in,OutputStream out) 
 	{
 		super();
-		System.out.println("constractor of cli()");
-		propertyChangeHandler=new PropertyChangeSupport(this);
+		this.propertyChangeHandler=new PropertyChangeSupport(this);
 		this.in=new BufferedReader(new InputStreamReader(in));
 		this.out=new BufferedWriter(new OutputStreamWriter(out));
 	}
 	
+	/**
+	 * @param pcl - object that wants to be added to the list of CLI commands observers
+	 */
 	public void addPropertyChangeListener(PropertyChangeListener pcl)
 	{
-		propertyChangeHandler.addPropertyChangeListener(pcl);
-		System.out.println("addPropertyChangeListener() in cli->"+pcl+" is listening to cli");
+		this.propertyChangeHandler.addPropertyChangeListener(pcl);
 	}
 	
+	/**
+	 * @param pcl - objects that wants to be removed from the list of CLI observers
+	 */
 	public void removePropertyChangeListener(PropertyChangeListener pcl)
 	{
 		propertyChangeHandler.removePropertyChangeListener(pcl);
 	}
 	
+	/**
+	 * @param response - response to a command that was last received
+	 */
 	public void writeResponse(String response)
 	{
 		try 
@@ -49,67 +73,105 @@ public class CLI implements Runnable {
 		}
 	}
 
+	/**
+	 * run in interface java.lang.Runnable
+	 */
 	@Override
 	public void run() 
-	{	
-		System.out.println("run() of cli");
-		String request=null;
-		String[] commandArr=new String[2];
-		while(true)
+	{
+		InputValidation result=InputValidation.INVALID;
+		while(result!=InputValidation.SHUTDOWN)
 		{
-			try 
-			{
-				out.write("Please enter your command\n");
-				out.flush();
-				
-				request=(String)in.readLine();
-				commandArr=request.split(" ");
-				
-				if(commandArr[0].equals("GAME_SERVER_CONFIG"))
+				writeResponse("Please enter your command\n");
+				String input = null;
+				try 
 				{
-					if(commandArr.length==2)
-					{
-						try
-						{
-							int capacity=Integer.parseInt(commandArr[1]);
-							propertyChangeHandler.firePropertyChange("GAME_SERVER_CONFIG",0,capacity);
-							writeResponse("The server can run "+capacity+" games concurrently\n");
-						}
-						catch(Exception e)
-						{
-							writeResponse("Not a valid command (problem with the capacity...)\n");
-						}
-					}
-					else if(commandArr.length==1)
-					{
-						propertyChangeHandler.firePropertyChange("GAME_SERVER_CONFIG",0,2);
-						writeResponse("The server can run 2 games concurrently by default\n");
-					}
-					else
-					{
-						writeResponse("Not a valid command\n");
-					}
+					input = (String)in.readLine();
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
 				}
-				else if(commandArr[0].equals("START"))
-				{
-					propertyChangeHandler.firePropertyChange("START",0,1);
+				result=inputValidation(input);
+				switch(result) {
+				case CONFIG:
+					int capacity=Integer.parseInt(input.split(" ")[1]);
+					this.propertyChangeHandler.firePropertyChange("GAME_SERVER_CONFIG", 0, capacity);
+					writeResponse("The server can run "+capacity+" games concurrently\n");
+					break;
+				case DEFAULT:
+					this.propertyChangeHandler.firePropertyChange("GAME_SERVER_CONFIG",  0, 2);
+					writeResponse("The server can run 2 games concurrently by default\n");
+					break;
+				case START:
+					this.propertyChangeHandler.firePropertyChange("START", 0, 1);
 					writeResponse("Strarting server.....\n");
-				}
-				else if(commandArr[0].equals("SHUTDOWN"))
-				{
-					propertyChangeHandler.firePropertyChange("SHUTDWON",0,1);
-					writeResponse("Shutdown server\n");
+					break;
+				case INVALID:
+					writeResponse("Not a valid command.\n"
+							+ "Legal commands: \n"
+							+ "'START' - start the server\n"
+							+ "'GAME_SERVER_CONFIG {integer}'- config the server in how many games he can support in parallel\n"
+							+ "'GAME_SERVER_CONFIG' - default configuration : the server will be able to support 2 games parallel\n"
+							+ "'SHUTDWON' - shut down the server");
+					break;
+				default:
 					break;
 				}
-				else
-				{
-					writeResponse("Not a valid command\n");
-				}
-			} 
-			catch (Exception e) 
-			{
-				e.printStackTrace();
-			}
 		}
+	}
+
+	/**
+	 * validation of the input entered by the client
+	 * @param input - The command entered by the client
+	 * @return enum InputValidation
+	 */
+	public InputValidation inputValidation(String input)
+	{
+		InputValidation result = InputValidation.INVALID;
+		String[] requestAsArray=new String[2];
+		requestAsArray=input.split(" ");
+		if(requestAsArray.length>2)
+			result=InputValidation.INVALID;
+		if(requestAsArray.length==0)
+			result=InputValidation.INVALID;
+		if(requestAsArray.length==2)
+			if(requestAsArray[0].equals("GAME_SERVER_CONFIG") && isNumeric(requestAsArray[1]))
+				result=InputValidation.CONFIG;
+			else
+				result=InputValidation.INVALID;
+		if(requestAsArray.length==1)
+		{
+			if(requestAsArray[0].equals("START"))
+				result=InputValidation.START;
+			else if(requestAsArray[0].equals("SHUTDWON"))
+				result=InputValidation.SHUTDOWN;
+			else if(requestAsArray[0].equals("GAME_SERVER_CONFIG"))
+				result=InputValidation.DEFAULT;
+		}
+		return result;
+	}
+	
+	/**
+	 * check if the client entered valid number 
+	 * @param strNum - the string we want to check
+	 * @return true if it is valid number false otherwise
+	 * Taken from GeeksForGeeks
+	 */
+	public static boolean isNumeric(String strNum) 
+	{
+	    if (strNum == null) 
+	    {
+	        return false;
+	    }
+	    try 
+	    {
+	        double d = Integer.parseInt(strNum);
+	    } 
+	    catch (NumberFormatException nfe) 
+	    {
+	        return false;
+	    }
+	    return true;
 	}
 }
