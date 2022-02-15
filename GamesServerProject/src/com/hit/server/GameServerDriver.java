dc8a8211e3ef9fb14953f7ad36c15a140d51e32c
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 14:29:04 +0200

    Update GameServerDriver.java

--- GamesServerProject/src/com/hit/server/GameServerDriver.java
+++ GamesServerProject/src/com/hit/server/GameServerDriver.java
@@ -2,13 +2,11 @@ package com.hit.server;
 
 import com.hit.util.CLI;
 
+/**
+ * This class is actually the part that connects all the system components to us and also activates them
+ */
 public class GameServerDriver {
 	
-	public GameServerDriver()
-	{
-		
-	}
-
 	public static void main(String[] args) 
 	{
 		CLI cli=new CLI(System.in,System.out);
