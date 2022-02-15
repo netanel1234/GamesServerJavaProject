29f07952b3874fdaf88de13616911742449e8c12
Netanel Elikamel <57944316+netanel1234@users.noreply.github.com>
2022-02-15 13:20:13 +0200

    Update Model.java

--- GamesClientProject/src/com/hit/model/Model.java
+++ GamesClientProject/src/com/hit/model/Model.java
@@ -2,10 +2,6 @@ package com.hit.model;
 
 public interface Model {
 	
-	//Sends the server a "new-game" request
 	void newGame(String gameType,String opponentType);
-	
-	//Sends the server an "update-move" message
 	void updatePlayerMove(int row,int col);
-	
 }
